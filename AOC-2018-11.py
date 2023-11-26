ee32a3ff9dc490254011d315719014141723db2d
Matthew McDougal <24367033+mmdoogie@users.noreply.github.com>
2023-11-25 21:43:16 -0600

    Faster Day 11 Part 2

--- AOC-2018-11.py
+++ AOC-2018-11.py
@@ -24,7 +24,7 @@ def part1(output = True):
     for y in range(1, 301 - 2):
         for x in range(1, 301 - 2):
             pts = [(x, y    ), (x + 1, y    ), (x + 2, y    ),
-                   (x, y + 1), (x + 1, y + 1), (x + 2, y + 2),
+                   (x, y + 1), (x + 1, y + 1), (x + 2, y + 1),
                    (x, y + 2), (x + 1, y + 2), (x + 2, y + 2)]
             total_level = sum([levels[p] for p in pts])
             if total_level > max_level:
@@ -43,31 +43,42 @@ def part2(output = True):
 
     max_level = 0
     max_pt = (0, 0, 0)
+
+    row_sums = [[0]]
+    col_sums = []
+    for x in range(301):
+        col_sums += [[0]]
+
+    for y in range(1, 301):
+        row = [0]
+        rs = 0
+        col_sums[0] += [0]
+        for x in range(1, 301):
+            col_sums[x] += [col_sums[x][-1] + levels[(x, y)]]
+            rs += levels[(x, y)]
+            row += [rs]
+        row_sums += [row]
+
     for y in range(1, 301 - 2):
         for x in range(1, 301 - 2):
             total_level = 0
-            for dy in range(3):
-                for dx in range(3):
-                    total_level += levels[(x + dx, y + dy)]
+            for wind in range(3):
+                total_level += row_sums[y + wind][x + 2] - row_sums[y + wind][x - 1]
             if total_level > max_level:
                 if output:
                     print(f'At {x},{y},3 level={total_level}')
                 max_level = total_level
                 max_pt = (x, y, 3)
             while True:
-                if x + dx + 1 > 300 or y + dy + 1 > 300:
+                if x + wind + 1 > 300 or y + wind + 1 > 300:
                     break
-                for ddy in range(dy):
-                    total_level += levels[(x + dx + 1, y + ddy)]
-                for ddx in range(dx):
-                    total_level += levels[(x + ddx, y + dy + 1)]
-                total_level += levels[(x + dx + 1, y + dy + 1)]
+                total_level += row_sums[y + wind + 1][x + wind    ] - row_sums[y + wind + 1][x - 1]
+                total_level += col_sums[x + wind + 1][y + wind + 1] - col_sums[x + wind + 1][y - 1]
                 if total_level > max_level:
                     if output:
-                        print(f'At {x},{y},{dx + 2} level={total_level}')
+                        print(f'At {x},{y},{wind + 2} level={total_level}')
                     max_level = total_level
-                    max_pt = (x, y, dx + 2)
-                dx += 1
-                dy += 1
+                    max_pt = (x, y, wind + 2)
+                wind += 1
 
     return ','.join([str(v) for v in max_pt])
